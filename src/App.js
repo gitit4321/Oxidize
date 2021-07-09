7053136caf8234d33f70a95a92f747a3c7fcb9d2
Nate Moore <moorenat@oregonstate.edu>
2021-07-09 17:15:29 -0600

    First page practice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,38 @@
 //import './App.css';
+import React from 'react'
+
+const Title = () => {
+  return (
+    <h1>
+      Title
+    </h1>
+  )
+}
+
+const Instructions = () => {
+  return (
+    <h3>
+      Make a choice and live with the consequences
+    </h3>
+  )
+}
+
+
+const GameDiv = () => {
+  return (
+    <></>
+
+  )
+}
+
+const ChoiceA = () => {
+  return (
+    <></>
+  )
+}
+
+
+
 
 function App() {
   return (

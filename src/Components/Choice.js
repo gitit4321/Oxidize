ec14c283fbf9cbc55b481d40168a08a730156c2a
Andrew Dunn <dunnand@oregonstate.edu>
2021-07-09 19:08:16 -0500

    Broken GameState

--- src/Components/Choice.js
+++ src/Components/Choice.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-const Choice = () => {
+const Choice = (props) => {
     return (
-        <button></button>
+        <button>{props.value}</button>
     )
 }
 
--- src/Components/GameDiv.js
+++ src/Components/GameDiv.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Choice from './Choice'
 
 const GameDiv = () => {
+
+    const [gameState, setGameState] = useState(0);
+
+    useEffect((gameState) => {
+
+    });
+
+    let handleWrongAnswer = () => {
+       
+    }
+
     return (
         <div>
-            <Choice/>
-            <Choice/>
+            <Choice onClick={() =>  setGameState(gameState + 1)} value="Button A"/>
+            <Choice value="Button B"/>
+            <p>lives = {gameState}</p>
         </div>
 
     )
